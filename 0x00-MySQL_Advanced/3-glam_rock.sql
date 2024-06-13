07c6ad21abf269ebb8d1bdc07ecf82a523e070d2
anto-muli <memuli@students@uonbi.ac.ke>
2024-06-13 08:11:20 +0300

    Done

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,8 @@
+-- Lists bands with Glam rock as their style,
+-- ranked by longevity
+-- Column names must be: band_name & lifespan
+
+SELECT band_name, (IFNULL(split, '2022') - formed) AS lifespan
+    FROM metal_bands
+    WHERE style LIKE '%Glam rock%'
+    ORDER BY lifespan DESC;
