bbc0be8ced39fefe4234ea2a8038c617b028daef
anto-muli <memuli@students.uonbi.ac.ke>
2024-06-18 17:43:38 +0300

    done

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python3
-""" MongoDB Ops using pymongo"""
+'''Task 11's .
+'''
 
 
-def schools_by_top(mongo_collection, topic):
-    """ return a list of schools with specific topics"""
-    documents = mongo_collection.find({"topics": topic})
-    return list(documents)
+def schools_by_topic(mongo_collection, topic):
+    '''Returns a list of school having a specific topic.
+    '''
+    topic_filter = {
+        'topics': {
+            '$elemMatch': {
+                '$eq': topic,
+            },
+        },
+    }
+    return [doc for doc in mongo_collection.find(topic_filter)]
